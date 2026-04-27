96987c2d1d6ef76c3d9296231a0238406bf4d52f
Eleftheria Sfetkou <eleftheriasfetkou@gmail.com>
2026-04-27 23:37:28 +0300

    adding component navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
 import { Step2PersonTable } from './components/step2-person-table/step2-person-table';
 import { Step3PersonTableInput } from './components/step3-person-table-input/step3-person-table-input';
 import { Step4ForDirective } from './components/step4-for-directive/step4-for-directive';
@@ -13,7 +14,8 @@ import { IPerson } from './shared/interfaces/person';
     Step3PersonTableInput,
     Step4ForDirective,
     Step5EventBind,
-    RouterOutlet
+    RouterOutlet,
+    Navbar
 ],
   templateUrl: './app.html',
   styleUrl: './app.css'
