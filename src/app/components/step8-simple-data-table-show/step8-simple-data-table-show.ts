18f77af967d32b91a15fec2e10baedb07ebbba1e
Eleftheria Sfetkou <eleftheriasfetkou@gmail.com>
2026-04-27 23:56:24 +0300

    adding components steps 8

---
+++ src/app/components/step8-simple-data-table-show/step8-simple-data-table-show.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step8-simple-data-table-show',
+  imports: [],
+  templateUrl: './step8-simple-data-table-show.html',
+  styleUrl: './step8-simple-data-table-show.css',
+})
+export class Step8SimpleDataTableShow {}
---
+++ src/app/components/step8-simple-data-table/step8-simple-data-table.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step8-simple-data-table',
+  imports: [],
+  templateUrl: './step8-simple-data-table.html',
+  styleUrl: './step8-simple-data-table.css',
+})
+export class Step8SimpleDataTable {}
