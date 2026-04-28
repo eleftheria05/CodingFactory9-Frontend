6ec632470d942fa10f3d796fa5592784a3f42bc7
Eleftheria Sfetkou <eleftheriasfetkou@gmail.com>
2026-04-28 23:11:59 +0300

    adding component 10 -form

---
+++ src/app/components/step10-template-driven-forms/person-template-form/person-template-form.ts
@@ -0,0 +1,39 @@
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from "@angular/forms"
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select'; 
+import { IPerson } from '../../../shared/interfaces/person';
+
+@Component({
+  selector: 'app-person-template-form',
+  imports: [
+    FormsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  templateUrl: './person-template-form.html',
+  styleUrl: './person-template-form.css',
+})
+export class PersonTemplateForm {
+  @Output() person = new EventEmitter<IPerson>(); 
+  @ViewChild('eForm', {static:false}) form:NgForm | undefined;
+
+  onSubmit(value:any){
+    // console.log("Submit");
+    console.log("Form Submit", this.form?.form.get('firstname')!.value)
+    this.person.emit(value);
+  }
+
+  onSetValue(){
+    console.log("Set Value");
+    this.form?.setValue({
+      firstname: "Markos",
+      lastname: "Lalakis",
+      email:"markos@aueb.gr"
+    })
+  }
+}
