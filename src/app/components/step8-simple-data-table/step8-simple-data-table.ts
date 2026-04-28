218f725d0c5cc29f9fc78b422725f932f5f0c86a
Eleftheria Sfetkou <eleftheriasfetkou@gmail.com>
2026-04-28 22:33:56 +0300

    table of data with sort

--- src/app/components/step8-simple-data-table/step8-simple-data-table.ts
+++ src/app/components/step8-simple-data-table/step8-simple-data-table.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { IPerson } from '../../shared/interfaces/person';
+import { sortBy } from 'lodash-es';
 
 @Component({
   selector: 'app-step8-simple-data-table',
@@ -7,6 +8,39 @@ import { IPerson } from '../../shared/interfaces/person';
   templateUrl: './step8-simple-data-table.html',
   styleUrl: './step8-simple-data-table.css',
 })
+
 export class Step8SimpleDataTable {
   @Input() data:IPerson[] | undefined;
+  //@Output() personClicked = new EventEmitter<IPerson>()
+
+  ngOnInit(){
+    // console.log("NG ON INIT 1>>>", this.sortOrder);
+    // this.sortOrder['firstname'] = 'asc';
+    // this.data = sortBy(this.data, 'firstname');
+    // this.sortData('firstname');
+    // /console.log("NG ON INIT 2>>>", this.sortOrder);
+  }
+
+  sortOrder = {
+    firstname: 'none',
+    lastname: 'none',
+    email:'none'
+  };
+
+  sortData(sortKey: keyof IPerson) {
+    console.log("STEP 8", this.data);
+    console.log("SortData", this.sortOrder[sortKey], sortKey);
+    if (this.sortOrder[sortKey] === 'asc') {
+      this.sortOrder[sortKey]='desc';
+      this.data = sortBy(this.data, sortKey).reverse();
+    } else {
+      this.sortOrder[sortKey] = 'asc';
+      this.data = sortBy(this.data, sortKey);
+    }
+    console.log("SortOrder", this.sortOrder);
+  }
+
+  //onPersonClicked(data: IPerson) {
+    //this.personClicked.emit(data);
+  //}
 }
